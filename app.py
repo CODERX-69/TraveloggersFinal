605df7340215250803db9dacd08cc7033d1f234f
Ashutosh Kumbhar <ashutoshkumbhar@Ashutoshs-MacBook-Air.local>
2023-06-01 15:52:58 +0530

    done

--- app.py
+++ app.py
@@ -601,4 +601,4 @@ def get_password_strength(password):
         return 'weak'
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0")
